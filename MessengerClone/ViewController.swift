bcda979b6c33e0d79163781fa2148550088bd60c
Michael Handkins <michaelhandkins@gmail.com>
2021-01-16 14:12:00 -0600

    Initial commit

---
+++ MessengerClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessengerClone
+//
+//  Created by Michael Handkins on 1/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
