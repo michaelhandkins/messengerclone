4ddba41cc814e6e9699e37b5c55f2975829a1b52
Michael Handkins <michaelhandkins@gmail.com>
2021-01-17 12:51:05 -0600

    Set up login and register view UI

--- MessengerClone/Controllers/ConversationsViewController.swift
+++ MessengerClone/Controllers/ConversationsViewController.swift
@@ -11,9 +11,24 @@ class CoversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "loggedIn")
+        
+        if isLoggedIn == false {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
     }
 
-
+    
+    
+    
 }
 
