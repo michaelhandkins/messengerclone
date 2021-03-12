a9ae8d6f92db1fbf4fdcc4b950aa1f56d32f54c0
Michael Handkins <michaelhandkins@gmail.com>
2021-03-11 19:46:24 -0600

    Begin setting up the chat view and new chat view by bringing in MessageKit and implementing some functionality

--- MessengerClone/Controllers/ChatViewController.swift
+++ MessengerClone/Controllers/ChatViewController.swift
@@ -6,13 +6,63 @@
 //
 
 import UIKit
+import MessageKit
 
-class ChatViewController: UIViewController {
+struct Message: MessageType {
+    var sender: SenderType
+    var messageId: String
+    var sentDate: Date
+    var kind: MessageKind
+    
+}
 
+struct Sender: SenderType {
+    var photoURL: String
+    var senderId: String
+    var displayName: String
+    
+}
+
+class ChatViewController: MessagesViewController {
+
+    private var messages = [Message]()
+    
+    private let selfSender = Sender(photoURL: "",
+                                    senderId: "1",
+                                    displayName: "Michael Berean")
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        messages.append(Message(sender: selfSender,
+                                messageId: "1",
+                                sentDate: Date(),
+                                kind: .text("Hello World message")))
+        messages.append(Message(sender: selfSender,
+                                messageId: "1",
+                                sentDate: Date(),
+                                kind: .text("Hello World message. Hello World message.")))
 
         view.backgroundColor = .red
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
     }
 
+}
+
+extension ChatViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+    func currentSender() -> SenderType {
+        return selfSender
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+    
+    
 }
--- MessengerClone/Controllers/ConversationsViewController.swift
+++ MessengerClone/Controllers/ConversationsViewController.swift
@@ -32,10 +32,17 @@ class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(didTapComposeButton))
         view.addSubview(tableView)
         view.addSubview(noConversationsLabel)
         setupTableView()
         fetchConversations()
+    }
+    
+    @objc private func didTapComposeButton() {
+        let vc = NewConversationViewController()
+        let navVC = UINavigationController(rootViewController: vc)
+        present(navVC, animated: true)
     }
     
     override func viewDidLayoutSubviews() {
